b8546c179c26e5ca0abcf914f4d5d1897bc01572
Fedor <144223030+FiodorSemenov@users.noreply.github.com>
2024-12-18 22:37:41 +0300

    Update task2.py

--- Lab_2/task2.py
+++ Lab_2/task2.py
@@ -1,14 +1,14 @@
 BOOKS_DATABASE = [
     {
         "id": 1,
-        "name": "test_name_1", "pages": 200,
-
+        "name": "test_name_1",
+        "pages": 200,
     },
     {
         "id": 2,
         "name": "test_name_2",
         "pages": 400,
-    }
+    },
 ]
 
 
@@ -30,7 +30,7 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f'Book(id_={self.id}, name=\'{self.name}\', pages={self.pages})'
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
 
 
 # TODO: написать класс Library
@@ -64,12 +64,14 @@ class Library:
         raise ValueError("Книги с запрашиваемым id не существует")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     empty_library = Library()
     print(empty_library.get_next_book_id())
 
-    list_books = [Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in
-                  BOOKS_DATABASE]
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
+        for book_dict in BOOKS_DATABASE
+    ]
     library_with_books = Library(books=list_books)
     print(library_with_books.get_next_book_id())
     print(library_with_books.get_index_by_book_id(1))
