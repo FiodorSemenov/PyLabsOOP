97e6bff15adc44fedd681ca2016f6787033fc97a
Fedor <144223030+FiodorSemenov@users.noreply.github.com>
2024-12-18 23:01:34 +0300

    Update task1.py

--- Lab_2/task1.py
+++ Lab_2/task1.py
@@ -1,14 +1,14 @@
 BOOKS_DATABASE = [
     {
         "id": 1,
-        "name": "test_name_1", "pages": 200,
-
+        "name": "test_name_1",
+        "pages": 200,
     },
     {
         "id": 2,
         "name": "test_name_2",
         "pages": 400,
-    }
+    },
 ]
 
 
@@ -35,8 +35,9 @@ class Book:
 
 if __name__ == '__main__':
     # инициализируем список книг
-    list_books = [Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in
-                  BOOKS_DATABASE]
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
     for book in list_books:
         print(book)  # проверяем метод __str__
 
