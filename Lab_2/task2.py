7e353c5badf2735e812cc8891224878e8aea53ef
Fedor <144223030+FiodorSemenov@users.noreply.github.com>
2024-12-18 21:48:35 +0300

    Update task2.py

--- Lab_2/task2.py
+++ Lab_2/task2.py
@@ -4,13 +4,15 @@ BOOKS_DATABASE = [{"id": 1, "name": "test_name_1", "pages": 200, }, {"id": 2, "n
 # TODO: написать класс Book
 class Book:
     def __init__(self, id_, name, pages):
-        '''
+        """
+
+
         Создание и подготовка к работе объекта "Книга"
 
         :param id_: id книги
         :param name: название книги
         :param pages: количество страниц
-        '''
+        """
         self.id = id_
         self.name = name
         self.pages = pages
@@ -25,28 +27,34 @@ class Book:
 # TODO: написать класс Library
 class Library:
     def __init__(self, books: list = None):
-        '''
+        """
+
+
         Создание и подготовка к работе объекта "Библиотека"
 
         :param books: список книг в библиотеке
-        '''
+        """
         self.books = books if books is not None else []
 
     def get_next_book_id(self):
-        '''
+        """
+
+
         Функция которая возвращает индекс для добавления новой книги
-        '''
+        """
         if len(self.books) == 0:
             return 1
         else:
             return self.books[-1].id + 1
 
     def get_index_by_book_id(self, book_id):
-        '''
+        """
+
+
         Функция которая возвращает индекс книги в списке
-        
+
         :param book_id: id книги
-        '''
+        """
         for index, book in enumerate(self.books):
             if book.id == book_id:
                 return index
